e2b9b0ff3615e02ba4d423bfd760088b380d9ea7
Saphereye <adarshdas950@gmail.com>
2024-02-22 23:51:34 +0530

    feat: Formatted many points together in fibonacci spiral
    Removed input system

--- src/main.rs
+++ src/main.rs
@@ -3,66 +3,56 @@ use bevy::{
     prelude::*,
     sprite::{MaterialMesh2dBundle, Mesh2dHandle},
 };
-
-#[derive(Component)]
-struct MainCamera;
-
-#[derive(Component)]
-struct Node {
-    x_position: f32,
-    y_position: f32,
-}
+use bevy_egui::{egui, EguiContexts, EguiPlugin};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(EguiPlugin)
         .add_systems(Startup, setup)
-        .add_systems(Update, mouse_click_system)
+        .add_systems(Update, ui)
         .run();
 }
 
-fn mouse_click_system(
-    mut commands: Commands,
-    mouse_button_input: Res<ButtonInput<MouseButton>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-    mut meshes: ResMut<Assets<Mesh>>,
-    window_query: Query<&Window, With<PrimaryWindow>>,
-    camera_query: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
-) {
-    let (camera, camera_transform) = camera_query.single();
-    let window = window_query.single();
-
-    if let Some(world_position) = window
-        .cursor_position()
-        .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor))
-        .map(|ray| ray.origin.truncate())
-    {
-        info!("World coords: {}/{}", world_position.x, world_position.y);
-
-        if mouse_button_input.just_pressed(MouseButton::Left) {
-            info!("left mouse just pressed");
-            commands.spawn((
-                MaterialMesh2dBundle {
-                    mesh: bevy::sprite::Mesh2dHandle(meshes.add(Circle { radius: 50.0 })),
-                    material: materials.add(Color::hsl(1.0, 0.95, 0.7)),
-                    transform: Transform::from_xyz(world_position.x as f32, world_position.y as f32, 0.0),
-                    ..default()
-                },
-                Node {
-                    x_position: world_position.x as f32,
-                    y_position: world_position.y as f32,
-                },
-            ));
-        }
-    }
-}
-
 const X_EXTENT: f32 = 600.;
+const MAX_RADIUS: f32 = 50.0;
 
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    commands.spawn((Camera2dBundle::default(), MainCamera));
+    commands.spawn(Camera2dBundle::default());
+    let mut shapes = vec![];
+    for _i in 0..100 {
+        shapes.push(Mesh2dHandle(meshes.add(Circle { radius: 10.0 })));
+    }
+    let num_shapes = shapes.len();
+
+    let golden_angle = 137.5_f32.to_radians();
+
+    for (i, shape) in shapes.into_iter().enumerate() {
+        let index: f32 = (i as f32) - (i as f32)/2.0;
+        // Distribute colors evenly across the rainbow.
+        let color = Color::hsl(360. * i as f32 / num_shapes as f32, 0.95, 0.7);
+
+        let angle = 2.0 * 3.14 * index * (1.0 / golden_angle);
+        let radius = MAX_RADIUS * (index - 0.5).sqrt();
+
+        let x = angle.cos() * radius;
+        let y = angle.sin() * radius;
+
+        commands.spawn(MaterialMesh2dBundle {
+            mesh: shape,
+            material: materials.add(color),
+            transform: Transform::from_xyz(x, y, 0.0),
+            ..default()
+        });
+    }
+}
+
+fn ui(mut contexts: EguiContexts) {
+    egui::SidePanel::left("side_panel").show(contexts.ctx_mut(), |ui| {
+        ui.label("side panel");
+    });
 }

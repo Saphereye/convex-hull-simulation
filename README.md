ba40537561a1f19a4f5c793fb478ad550a3d1e5a
Saphereye <adarshdas950@gmail.com>
2024-02-22 22:13:59 +0530

    feat: Added readme and short demo

---
+++ README.md
@@ -0,0 +1,17 @@
+# [Convex Hull](https://en.wikipedia.org/wiki/Convex_hull) Simulation
+
+## Prereqs
+- Install [cargo](https://doc.rust-lang.org/cargo/getting-started/installation.html)
+- [Learn](https://doc.rust-lang.org/book/) Rust (Optional)
+- Go through [bevy examples](https://github.com/bevyengine/bevy/tree/latest/examples#examples)
+
+## How to run
+- Install [trunk](https://trunkrs.dev/) using `cargo install --locked trunk` or if you are feeling fancy
+```bash
+wget -qO- https://github.com/trunk-rs/trunk/releases/download/0.17.10/trunk-x86_64-unknown-linux-gnu.tar.gz | tar -xzf-
+``` 
+- Run `trunk serve`. This will first compile the program (which will take a lot of time at first)
+- After that's complete the application will be hosted on `127.0.0.1:8080`
+
+## Additional comments
+- I have added the ithub pages integration so the application will be hosted on github directly now
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,60 @@
+use bevy::window::PrimaryWindow;
 use bevy::{
     prelude::*,
     sprite::{MaterialMesh2dBundle, Mesh2dHandle},
 };
 
+#[derive(Component)]
+struct MainCamera;
+
+#[derive(Component)]
+struct Node {
+    x_position: f32,
+    y_position: f32,
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(Update, mouse_click_system)
         .run();
+}
+
+fn mouse_click_system(
+    mut commands: Commands,
+    mouse_button_input: Res<ButtonInput<MouseButton>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    camera_query: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
+) {
+    let (camera, camera_transform) = camera_query.single();
+    let window = window_query.single();
+
+    if let Some(world_position) = window
+        .cursor_position()
+        .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor))
+        .map(|ray| ray.origin.truncate())
+    {
+        info!("World coords: {}/{}", world_position.x, world_position.y);
+
+        if mouse_button_input.just_pressed(MouseButton::Left) {
+            info!("left mouse just pressed");
+            commands.spawn((
+                MaterialMesh2dBundle {
+                    mesh: bevy::sprite::Mesh2dHandle(meshes.add(Circle { radius: 50.0 })),
+                    material: materials.add(Color::hsl(1.0, 0.95, 0.7)),
+                    transform: Transform::from_xyz(world_position.x as f32, world_position.y as f32, 0.0),
+                    ..default()
+                },
+                Node {
+                    x_position: world_position.x as f32,
+                    y_position: world_position.y as f32,
+                },
+            ));
+        }
+    }
 }
 
 const X_EXTENT: f32 = 600.;
@@ -17,36 +64,5 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    commands.spawn(Camera2dBundle::default());
-
-    let shapes = [
-        Mesh2dHandle(meshes.add(Circle { radius: 100.0 })),
-        Mesh2dHandle(meshes.add(Ellipse::new(25.0, 50.0))),
-        Mesh2dHandle(meshes.add(Capsule2d::new(25.0, 50.0))),
-        Mesh2dHandle(meshes.add(Rectangle::new(50.0, 100.0))),
-        Mesh2dHandle(meshes.add(RegularPolygon::new(50.0, 6))),
-        Mesh2dHandle(meshes.add(Triangle2d::new(
-            Vec2::Y * 50.0,
-            Vec2::new(-50.0, -50.0),
-            Vec2::new(50.0, -50.0),
-        ))),
-    ];
-    let num_shapes = shapes.len();
-
-    for (i, shape) in shapes.into_iter().enumerate() {
-        // Distribute colors evenly across the rainbow.
-        let color = Color::hsl(360. * i as f32 / num_shapes as f32, 0.95, 0.7);
-
-        commands.spawn(MaterialMesh2dBundle {
-            mesh: shape,
-            material: materials.add(color),
-            transform: Transform::from_xyz(
-                // Distribute shapes from -X_EXTENT to +X_EXTENT.
-                -X_EXTENT / 2. + i as f32 / (num_shapes - 1) as f32 * X_EXTENT,
-                0.0,
-                0.0,
-            ),
-            ..default()
-        });
-    }
+    commands.spawn((Camera2dBundle::default(), MainCamera));
 }

b6c64d72c9b4b65aec45d7747c02c28b6728eadf
Saphereye <adarshdas950@gmail.com>
2024-02-23 15:54:13 +0530

    feat: Added skeleton for making mesh

--- README.md
+++ README.md
@@ -10,8 +10,7 @@
 ```bash
 wget -qO- https://github.com/trunk-rs/trunk/releases/download/0.17.10/trunk-x86_64-unknown-linux-gnu.tar.gz | tar -xzf-
 ``` 
-- Run `trunk serve`. This will first compile the program (which will take a lot of time at first)
-- After that's complete the application will be hosted on `127.0.0.1:8080`
+- Run `cargo run`. This will first compile the program (which will take a lot of time the first time its compiled)
 
 ## Additional comments
-- I have added the ithub pages integration so the application will be hosted on github directly now
+- I have added the github pages integration so the application will be hosted on github directly now
--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,28 @@ use bevy::{
     sprite::{MaterialMesh2dBundle, Mesh2dHandle},
 };
 use bevy_egui::{egui, EguiContexts, EguiPlugin};
+use bevy_pancam::{PanCam, PanCamPlugin};
+use rayon::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugins(EguiPlugin)
+        .add_plugins((DefaultPlugins, EguiPlugin, PanCamPlugin::default()))
         .add_systems(Startup, setup)
         .add_systems(Update, ui)
         .insert_resource(NumberOfPoints::default())
+        .insert_resource(PointData::default())
         .run();
 }
 
-const MAX_RADIUS: f32 = 50.0;
-
 fn setup(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default());
+    commands.spawn(Camera2dBundle::default()).insert(PanCam {
+        grab_buttons: vec![MouseButton::Left, MouseButton::Middle], // which buttons should drag the camera
+        enabled: true,        // when false, controls are disabled. See toggle example.
+        zoom_to_cursor: true, // whether to zoom towards the mouse or the center of the screen
+        min_scale: 1.,        // prevent the camera from zooming too far in
+        max_scale: Some(40.), // prevent the camera from zooming too far out
+        ..default()
+    });
 }
 
 #[derive(Component)]
@@ -26,9 +33,18 @@ struct PointSingle(Vec2);
 #[derive(Resource)]
 struct NumberOfPoints(usize);
 
+#[derive(Resource)]
+struct PointData(Vec<Vec2>);
+
 impl Default for NumberOfPoints {
     fn default() -> Self {
         Self(10)
+    }
+}
+
+impl Default for PointData {
+    fn default() -> Self {
+        Self(vec![])
     }
 }
 
@@ -39,32 +55,33 @@ fn ui(
     mut materials: ResMut<Assets<ColorMaterial>>,
     point_query: Query<Entity, With<PointSingle>>,
     mut number_of_points: ResMut<NumberOfPoints>,
+    mut point_data: ResMut<PointData>,
 ) {
     egui::Window::new("Inspector").show(contexts.ctx_mut(), |ui| {
-        ui.add(egui::Slider::new(&mut number_of_points.0, 0..=100).text("Number of points"));
-
-        // TODO Add button to draw convex hull
+        ui.add(egui::Slider::new(&mut number_of_points.0, 0..=1_000).text("Number of points"));
 
         if ui.add(egui::Button::new("Generate World")).clicked() {
             for entity in point_query.iter() {
                 commands.entity(entity).despawn();
             }
+            point_data.0.clear();
 
             let num_shapes = number_of_points.0;
 
             let golden_angle = 137.5_f32.to_radians();
 
-            // TODO Use rayon::par_iter to blit the shapes
-            for i in 0..num_shapes {
+            (0..num_shapes).into_iter().for_each(|i| {
                 let index: f32 = (i as f32) - (i as f32) / 2.0;
                 // Distribute colors evenly across the rainbow.
                 let color = Color::hsl(360. * i as f32 / num_shapes as f32, 0.95, 0.7);
 
                 let angle = 2.0 * std::f32::consts::PI * index * (1.0 / golden_angle);
-                let radius = MAX_RADIUS * (index - 0.5).sqrt();
+                let radius = 100.0 * (index - 0.5).sqrt();
 
                 let x = angle.cos() * radius;
                 let y = angle.sin() * radius;
+
+                point_data.0.push(Vec2::new(x, y));
 
                 commands.spawn((
                     MaterialMesh2dBundle {
@@ -75,7 +92,11 @@ fn ui(
                     },
                     PointSingle(Vec2::new(x, y)),
                 ));
-            }
+            })
+        }
+
+        if ui.add(egui::Button::new("Generate Mesh")).clicked() {
+            println!("Points in space: {:?}", point_data.0);
         }
     });
 }

b2437184cd4a3a0b0d60413b18bf2c57f37cccf7
Saphereye <adarshdas950@gmail.com>
2024-02-23 13:39:48 +0530

    feat: Added slider for choosing number of points

--- src/main.rs
+++ src/main.rs
@@ -10,47 +10,72 @@ fn main() {
         .add_plugins(EguiPlugin)
         .add_systems(Startup, setup)
         .add_systems(Update, ui)
+        .insert_resource(NumberOfPoints::default())
         .run();
 }
 
 const MAX_RADIUS: f32 = 50.0;
 
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
+fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
-    let mut shapes = vec![];
-    for _i in 0..100 {
-        shapes.push(Mesh2dHandle(meshes.add(Circle { radius: 10.0 })));
-    }
-    let num_shapes = shapes.len();
+}
 
-    let golden_angle = 137.5_f32.to_radians();
+#[derive(Component)]
+struct PointSingle(Vec2);
 
-    for (i, shape) in shapes.into_iter().enumerate() {
-        let index: f32 = (i as f32) - (i as f32)/2.0;
-        // Distribute colors evenly across the rainbow.
-        let color = Color::hsl(360. * i as f32 / num_shapes as f32, 0.95, 0.7);
+#[derive(Resource)]
+struct NumberOfPoints(usize);
 
-        let angle = 2.0 * std::f32::consts::PI * index * (1.0 / golden_angle);
-        let radius = MAX_RADIUS * (index - 0.5).sqrt();
-
-        let x = angle.cos() * radius;
-        let y = angle.sin() * radius;
-
-        commands.spawn(MaterialMesh2dBundle {
-            mesh: shape,
-            material: materials.add(color),
-            transform: Transform::from_xyz(x, y, 0.0),
-            ..default()
-        });
+impl Default for NumberOfPoints {
+    fn default() -> Self {
+        Self(10)
     }
 }
 
-fn ui(mut contexts: EguiContexts) {
-    egui::SidePanel::left("side_panel").show(contexts.ctx_mut(), |ui| {
-        ui.label("side panel");
+fn ui(
+    mut contexts: EguiContexts,
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    point_query: Query<Entity, With<PointSingle>>,
+    mut number_of_points: ResMut<NumberOfPoints>,
+) {
+    egui::Window::new("Inspector").show(contexts.ctx_mut(), |ui| {
+        ui.add(egui::Slider::new(&mut number_of_points.0, 0..=100).text("Number of points"));
+
+        // TODO Add button to draw convex hull
+
+        if ui.add(egui::Button::new("Generate World")).clicked() {
+            for entity in point_query.iter() {
+                commands.entity(entity).despawn();
+            }
+
+            let num_shapes = number_of_points.0;
+
+            let golden_angle = 137.5_f32.to_radians();
+
+            // TODO Use rayon::par_iter to blit the shapes
+            for i in 0..num_shapes {
+                let index: f32 = (i as f32) - (i as f32) / 2.0;
+                // Distribute colors evenly across the rainbow.
+                let color = Color::hsl(360. * i as f32 / num_shapes as f32, 0.95, 0.7);
+
+                let angle = 2.0 * std::f32::consts::PI * index * (1.0 / golden_angle);
+                let radius = MAX_RADIUS * (index - 0.5).sqrt();
+
+                let x = angle.cos() * radius;
+                let y = angle.sin() * radius;
+
+                commands.spawn((
+                    MaterialMesh2dBundle {
+                        mesh: Mesh2dHandle(meshes.add(Circle { radius: 10.0 })),
+                        material: materials.add(color),
+                        transform: Transform::from_xyz(x, y, 0.0),
+                        ..default()
+                    },
+                    PointSingle(Vec2::new(x, y)),
+                ));
+            }
+        }
     });
 }

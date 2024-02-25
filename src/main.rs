771cd0f955a604aa29ef0f5882d8899c79e2409b
Saphereye <adarshdas950@gmail.com>
2024-02-25 15:01:00 +0530

    feat: Added animation to drawing the vis

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,27 @@ use bevy::{
 };
 use bevy_egui::{egui, EguiContexts, EguiPlugin};
 use bevy_pancam::{PanCam, PanCamPlugin};
-use rayon::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins((DefaultPlugins, EguiPlugin, PanCamPlugin::default()))
         .add_systems(Startup, setup)
         .add_systems(Update, ui)
-        .insert_resource(NumberOfPoints::default())
-        .insert_resource(PointData::default())
-        .insert_resource(Distribution::default())
+        .add_systems(Update, drawing_ui)
+        .insert_resource(NumberOfPoints(10))
+        .insert_resource(PointData(vec![]))
+        .insert_resource(Distribution(DistributionType::Fibonacci))
+        .insert_resource(DrawingInProgress(false, 0, 0))
+        .insert_resource(SimulationTimeSec(1.0))
+        .insert_resource(SimulationTimer(Timer::from_seconds(1.0, TimerMode::Repeating)))
         .run();
 }
+
+#[derive(Component)]
+struct Gizmo;
+
+#[derive(Component)]
+struct FinalLine;
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default()).insert(PanCam {
@@ -45,27 +54,117 @@ struct ConvexHull;
 #[derive(Resource)]
 struct NumberOfPoints(usize);
 
+#[derive(Resource)]
+struct SimulationTimeSec(f32);
+
+#[derive(Resource)]
+struct DrawingInProgress(bool, usize, usize); // (is_drawing, number of iterations reached, initial state of the drawing)
+
 #[derive(Resource, Debug)]
 struct PointData(Vec<Vec2>);
 
 #[derive(Resource)]
 struct Distribution(DistributionType);
 
-impl Default for NumberOfPoints {
-    fn default() -> Self {
-        Self(10)
-    }
-}
+#[derive(Resource)]
+struct SimulationTimer(Timer);
 
-impl Default for PointData {
-    fn default() -> Self {
-        Self(vec![])
+fn drawing_ui(
+    mut contexts: EguiContexts,
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    point_query: Query<Entity, With<PointSingle>>,
+    convex_hull_query: Query<Entity, With<ConvexHull>>,
+    mut number_of_points: ResMut<NumberOfPoints>,
+    mut point_data: ResMut<PointData>,
+    mut distribution: ResMut<Distribution>,
+    mut gizmo_query: Query<Entity, With<Gizmo>>,
+    mut drawing_in_progress: ResMut<DrawingInProgress>,
+    mut simulation_time: ResMut<SimulationTimeSec>,
+    time: Res<Time>,
+    mut simulation_timer: ResMut<SimulationTimer>,
+) {
+    if point_data.0.len() < 1 {
+        return;
     }
-}
 
-impl Default for Distribution {
-    fn default() -> Self {
-        Self(DistributionType::Fibonacci)
+    if !drawing_in_progress.0 {
+        return;
+    }
+
+    simulation_timer.0.tick(time.delta());
+    if !simulation_timer.0.finished() {
+        return;
+    }
+
+    let points = point_data.0.clone();
+
+    let mut current = drawing_in_progress.1;
+
+    let previous_point = [points[current].x as f32, points[current].y as f32, 0.0];
+
+    let mut next = (current + 1) % points.len();
+
+    for (i, point) in points.iter().enumerate() {
+        commands.spawn((
+            MaterialMesh2dBundle {
+                mesh: Mesh2dHandle(
+                    meshes.add(
+                        Mesh::new(PrimitiveTopology::LineStrip, RenderAssetUsages::default())
+                            .with_inserted_attribute(
+                                Mesh::ATTRIBUTE_POSITION,
+                                vec![
+                                    [previous_point[0], previous_point[1], 0.0],
+                                    [point.x as f32, point.y as f32, 0.0],
+                                ],
+                            ),
+                    ),
+                ),
+                material: materials.add(Color::rgb(0.44, 0.44, 0.44)),
+                ..default()
+            },
+            Gizmo,
+        ));
+        if i != current && i != next {
+            let cross = (point.x - points[current].x) * (points[next].y - points[current].y)
+                - (point.y - points[current].y) * (points[next].x - points[current].x);
+            if cross < 0.0 {
+                next = i;
+            }
+        }
+    }
+
+    info!(
+        "Drawing final line from {:?} to {:?}",
+        previous_point, points[next]
+    );
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: Mesh2dHandle(
+                meshes.add(
+                    Mesh::new(PrimitiveTopology::LineStrip, RenderAssetUsages::default())
+                        .with_inserted_attribute(
+                            Mesh::ATTRIBUTE_POSITION,
+                            vec![
+                                [previous_point[0], previous_point[1], 0.0],
+                                [points[next].x as f32, points[next].y as f32, 0.0],
+                            ],
+                        ),
+                ),
+            ),
+            material: materials.add(Color::rgb(1.0, 1.0, 1.0)),
+            ..default()
+        },
+        FinalLine,
+    ));
+
+    current = next;
+    drawing_in_progress.1 = current;
+    if current == drawing_in_progress.2 {
+        drawing_in_progress.0 = false;
+        drawing_in_progress.1 = 0;
+        return;
     }
 }
 
@@ -79,16 +178,30 @@ fn ui(
     mut number_of_points: ResMut<NumberOfPoints>,
     mut point_data: ResMut<PointData>,
     mut distribution: ResMut<Distribution>,
+    mut gizmo_query: Query<Entity, With<Gizmo>>,
+    mut drawing_in_progress: ResMut<DrawingInProgress>,
+    mut simulation_time: ResMut<SimulationTimeSec>,
+    mut simulation_timer: ResMut<SimulationTimer>,
 ) {
     egui::Window::new("Inspector").show(contexts.ctx_mut(), |ui| {
         ui.add(egui::Slider::new(&mut number_of_points.0, 0..=1_000).text("Number of points"));
+        if ui
+            .add(egui::Slider::new(&mut simulation_time.0, 0.0..=1.0).text("Simulation time (s)"))
+            .changed()
+        {
+            simulation_timer
+                .0
+                .set_duration(std::time::Duration::from_secs_f32(simulation_time.0));
+        }
 
         if ui.add(egui::Button::new("Generate World")).clicked() {
             for entity in point_query.iter() {
                 commands.entity(entity).despawn();
             }
-            for entity in convex_hull_query.iter() {
-                commands.entity(entity).despawn();
+            if !drawing_in_progress.0 {
+                for entity in convex_hull_query.iter() {
+                    commands.entity(entity).despawn();
+                }
             }
             point_data.0.clear();
 
@@ -165,56 +278,20 @@ fn ui(
             });
 
         if ui.add(egui::Button::new("Generate Mesh")).clicked() {
-            for entity in convex_hull_query.iter() {
-                commands.entity(entity).despawn();
-            }
-
-            let vertices = jarvis_march(point_data.0.clone());
-
-            commands.spawn((
-                MaterialMesh2dBundle {
-                    mesh: Mesh2dHandle(
-                        meshes.add(
-                            Mesh::new(PrimitiveTopology::LineStrip, RenderAssetUsages::default())
-                                .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, vertices),
-                        ),
-                    ),
-                    material: materials.add(Color::rgb(1.0, 1.0, 1.0)),
-                    ..default()
-                },
-                ConvexHull,
-            ));
-        }
-    });
-}
-
-fn jarvis_march(points: Vec<Vec2>) -> Vec<[f32; 3]> {
-    let mut hull = vec![];
-    let mut start = 0;
-    for (i, point) in points.iter().enumerate() {
-        if point.x < points[start].x {
-            start = i;
-        }
-    }
-    let mut current = start;
-    let first_point = [points[current].x as f32, points[current].y as f32, 0.0];
-    loop {
-        hull.push([points[current].x as f32, points[current].y as f32, 0.0]);
-        let mut next = (current + 1) % points.len();
-        for (i, point) in points.iter().enumerate() {
-            if i != current && i != next {
-                let cross = (point.x - points[current].x) * (points[next].y - points[current].y)
-                    - (point.y - points[current].y) * (points[next].x - points[current].x);
-                if cross < 0.0 {
-                    next = i;
+            if !drawing_in_progress.0 {
+                for entity in convex_hull_query.iter() {
+                    commands.entity(entity).despawn();
+                }
+                drawing_in_progress.0 = true;
+                drawing_in_progress.1 = 0;
+                let points = point_data.0.clone();
+                for (i, point) in points.iter().enumerate() {
+                    if point.x < points[drawing_in_progress.1].x {
+                        drawing_in_progress.1 = i;
+                        drawing_in_progress.2 = i;
+                    }
                 }
             }
         }
-        current = next;
-        if current == start {
-            break;
-        }
-    }
-    hull.push(first_point);
-    hull
+    });
 }

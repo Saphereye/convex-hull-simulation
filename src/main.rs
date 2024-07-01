31cb6a97d2408544e20a32509b03afd39253b7cf
Saphereye <adarshdas950@gmail.com>
2024-07-01 10:50:21 +0530

    fix: temporary fix, removed point coords input

--- src/main.rs
+++ src/main.rs
@@ -595,7 +595,9 @@ fn ui(
         ui.checkbox(&mut point_data.4, "Manually add points by clicking");
         
         // ui.text_edit_multiline(&mut point_data.1);
-        ui.code_editor(&mut point_data.1);
+        
+        // Removed temporarily coz not working on web build
+        // ui.code_editor(&mut point_data.1);
         
         if ui.button("Clear world").clicked() {
             despawn_entities(&mut commands, &point_query);
